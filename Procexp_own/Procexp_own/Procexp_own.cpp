f2f1ce0a952e360ab06d41f2e51935d3888171da
cheesechoi <cheese.choi@gmail.com>
2015-04-29 15:59:38 +0900

    Revert "findProcess"
    This reverts commit c41e7d7b19a8de9831ab11a75efc428f7fbefdd0.

--- Procexp_own/Procexp_own/Procexp_own.cpp
+++ Procexp_own/Procexp_own/Procexp_own.cpp
@@ -1,32 +1,16 @@
 // Procexp_own.cpp : Defines the entry point for the console application.
 //
 
-#pragma comment(lib, "Psapi.lib")
-
 #include "stdafx.h"
-#include <Windows.h>
-#include <Psapi.h>
-#include "EnumProcess.h"
 
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	DWORD _index = 0;
-	DWORD aProcesses[1024], cbNeeded, cProcesses;
-	_AboutProcess ap;
+	int _index = 0;
+	//EPROCESS eProc;
 
-	if (!EnumProcesses(aProcesses, sizeof(aProcesses), &cbNeeded))
-	{
-		return 1;
-	}
-	
-	cProcesses = cbNeeded / sizeof(DWORD);
 
-	
-	for (; _index < cProcesses; _index++)
-	{
-		ap.PrintProcessNameAndID(aProcesses[_index]);
-	}
+	printf("%d\n", _index++);
 
 	return 0;
 }
